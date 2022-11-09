ee6810a711b4896d7d977a52a0062141c5de5fc1
Laisses <laisse@gmail.com>
2022-11-09 08:58:37 -0300

    mongodb config

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
-const app = express();
 dotenv.config();
+const app = express();
+
 const PORT = process.env.PORT || 5000;
+const mongoClient = new MongoClient(process.env.DB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`server running on http://localhost:${PORT}`);
-});
+const main = async () => {
+    await mongoClient.connect();
+    const db = mongoClient.db("batePapoUol");
+    app.listen(PORT, () => {
+        console.log(`server running on http://localhost:${PORT}`);
+    });
+};
+
+main();
