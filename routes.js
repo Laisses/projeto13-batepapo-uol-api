29e30b620d66c62cd4aada8e9956b1be822676d7
Laisses <laisse@gmail.com>
2022-11-09 11:37:03 -0300

    routes.js: create get and post routes to participants

--- routes.js
+++ routes.js
@@ -1,5 +1,32 @@
 export const routes = (app, db) => {
-    app.get("/", (_req, res) => {
-        res.status(200).send("OK");
+    const participants = db.collection("participants");
+    const messages = db.collection("messages");
+
+    app.post("/participants", async (req, res) => {
+        const { name } = req.body;
+
+        await participants.insertOne({
+            name,
+            lastStatus: Date.now()
+        });
+
+        await messages.insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: Date.now()
+        });
+
+        res.sendStatus(201);
+    });
+
+    app.get("/participants", async (_req, res) => {
+
+        const users = await participants
+            .find()
+            .toArray();
+
+        res.status(200).send(users);
     });
 }
