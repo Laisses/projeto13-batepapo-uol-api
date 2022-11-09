c2bc73fe9d2327e476ef894d440a5ce9aa1eb4fa
Laisses <laisse@gmail.com>
2022-11-09 14:09:59 -0300

    routes.js: create get and post routes to messages

--- routes.js
+++ routes.js
@@ -34,11 +34,27 @@ export const routes = (app, db) => {
     });
 
     app.get("/participants", async (_req, res) => {
-
-        const users = await participants
-            .find()
-            .toArray();
-
+        const users = await participants.find().toArray();
         res.status(200).send(users);
+    });
+
+    app.post("/messages", async (req, res) => {
+        const { to, text, type } = req.body;
+        const from = req.headers.user;
+        await messages.insertOne({
+            from,
+            to,
+            text,
+            type,
+            time: dayjs(new Date()).format("HH:mm:ss")
+        });
+
+        res.sendStatus(201);
+    });
+
+    app.get("/messages", async (req, res) => {
+        const { limit } = req.query;
+        const newMessages = await messages.find().limit(Number(limit) || 0).toArray();
+        res.status(200).send(newMessages);
     });
 }
